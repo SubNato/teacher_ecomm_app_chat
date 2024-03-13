8d2151caf393043532f61f8e5515398b1834ec64
SubNato <ralphysub@gmail.com>
2024-03-13 17:54:25 -0500

    Updated information for the Server API URL.

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants{
   //static const String SERVER_API_URL = "https://2dc0-165-154-72-189.ngrok-free.app/";
-  static const String SERVER_API_URL = "/";
+  static const String SERVER_API_URL = "/";     //PLACE YOUR IP ADDRESS BEFORE THE SLASH ALONG WITH 'https:', OR YOUR DOMAIN NAME FOR THE APP TO ACTUALLY BE FUNCTIONAL AS WELL AS FOR THIS PART/SECTION TO BE COMPLETED!
   static const String SERVER_UPLOADS = "${SERVER_API_URL}uploads/";
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_open';
   static const String STORAGE_USER_PROFILE_KEY = 'user_profile_key';
